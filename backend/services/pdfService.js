0d30cc007f5ddb8399f3f23ba9a60b1000f045e8
Sohan238055 <sohannaik8055@gmail.com>
2026-08-17 00:52:49 +0530

    Add global polyfills for missing DOM APIs
    Add polyfills for DOMMatrix, ImageData, and Path2D.

--- backend/services/pdfService.js
+++ backend/services/pdfService.js
@@ -1,3 +1,13 @@
+if (typeof global.DOMMatrix === "undefined") {
+  global.DOMMatrix = class DOMMatrix {};
+}
+if (typeof global.ImageData === "undefined") {
+  global.ImageData = class ImageData {};
+}
+if (typeof global.Path2D === "undefined") {
+  global.Path2D = class Path2D {};
+}
+
 const { PDFParse } = require("pdf-parse");
 
 function toBuffer(value) {
